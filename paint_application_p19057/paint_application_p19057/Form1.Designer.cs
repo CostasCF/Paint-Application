d1649363a8f21d9f8b5b95fdb65d1704503be338
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2020-12-30 19:55:14 +0200

    Added DrawAction class

---
+++ paint_application_p19057/paint_application_p19057/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace paint_application_p19057
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawingCanvas = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.drawingCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawingCanvas
+            // 
+            this.drawingCanvas.BackColor = System.Drawing.Color.White;
+            this.drawingCanvas.Location = new System.Drawing.Point(76, 87);
+            this.drawingCanvas.Name = "drawingCanvas";
+            this.drawingCanvas.Size = new System.Drawing.Size(657, 297);
+            this.drawingCanvas.TabIndex = 0;
+            this.drawingCanvas.TabStop = false;
+            this.drawingCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.drawingCanvas_Paint);
+            this.drawingCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseDown);
+            this.drawingCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseMove);
+            this.drawingCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawingCanvas_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.drawingCanvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drawingCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawingCanvas;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ paint_application_p19057/paint_application_p19057/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace paint_application_p19057
+{
+    public partial class Form1 : Form
+    {
+      Pen pen;
+        DrawAction myaction = new DrawAction('P', Color.Blue, 3.3f);
+        List<DrawAction> actions = new List<DrawAction>();
+        List<List<Point>> allactions = new List<List<Point>>();
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black, 3);
+        }
+
+        private void drawingCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            actions.Add(myaction);
+            Draw(e.Graphics, actions);
+          
+        }
+
+        public void Draw(Graphics G, List<DrawAction> actions)
+        {
+            foreach (DrawAction da in actions)
+             if (da.type == 'P' && da.points.Count > 1)
+                    using (Pen pen = new Pen(da.color, da.penWidth))
+                        G.DrawLines(pen, da.points.ToArray());
+            // else..
+            foreach (List<Point> points in allactions)
+
+                if (points.Count > 1) G.DrawLines(pen, points.ToArray());
+
+        }
+
+        private void drawingCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+            drawingCanvas.Cursor = Cursors.Cross;
+            if (actions.Count > 1)
+            {
+                // begin fresh line or curve
+                myaction.points.Clear();
+                // startpoint
+                myaction.points.Add(e.Location);
+            }
+        }
+        private void drawingCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (actions.Count > 1)
+            {
+                // ToList creates a copy
+                allactions.Add(myaction.points.ToList());
+
+                myaction.points.Clear();
+            }
+        }
+
+        private void drawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+
+             if (e.Button != MouseButtons.Left) return;
+            // here we should check if the distance is more than a minimum!
+            myaction.points.Add(e.Location);
+            // let it show
+            drawingCanvas.Invalidate();
+        }
+
+        void AddTestActions()
+        {
+            actions.Add(new DrawAction('L', Color.Blue, 3.3f));
+            actions[0].points.Add(new Point(23, 34));
+            actions[0].points.Add(new Point(23, 134));
+            actions.Add(new DrawAction('P', Color.Red, 1.3f));
+            actions[1].points.Add(new Point(11, 11));
+            actions[1].points.Add(new Point(55, 11));
+            actions[1].points.Add(new Point(55, 77));
+            actions[1].points.Add(new Point(11, 77));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddTestActions();
+            Bitmap bmp = new Bitmap(drawingCanvas.ClientSize.Width,
+                                   drawingCanvas.ClientSize.Height);
+            using (Graphics G = Graphics.FromImage(bmp)) Draw(G, actions);
+            drawingCanvas.Image = bmp;
+        }
+
+      
+    }
+}
